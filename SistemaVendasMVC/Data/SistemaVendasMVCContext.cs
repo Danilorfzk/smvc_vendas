ff9cb15beca0b12ceaeb3580b7224de31ad1df8e
Danilorfzk <31334241+Danilorfzk@users.noreply.github.com>
2021-06-06 21:29:53 -0400

    CRUD Scaffolding

---
+++ SistemaVendasMVC/Data/SistemaVendasMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendasMVC.Models;
+
+namespace SistemaVendasMVC.Data
+{
+    public class SistemaVendasMVCContext : DbContext
+    {
+        public SistemaVendasMVCContext (DbContextOptions<SistemaVendasMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaVendasMVC.Models.Departamento> Departamento { get; set; }
+    }
+}
